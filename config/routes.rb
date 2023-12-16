036daa45b164edb344016e86483a5aa5b25134be
shome <yubemto12@gmail.com>
2023-12-16 21:16:33 +0900

    stpe1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index' # ルートURL('/')のルーティング
   get 'about', to: 'home#about' # '/about'へのルーティング
-
   resources :articles # articlesリソースに対する標準的なRESTfulルーティング
 end
